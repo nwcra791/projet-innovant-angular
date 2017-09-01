9acffe89fed7414396f2d01e20c2d31d447961ab
Lucas Pointurier <pointu_l@etna-alternance.net>
2017-09-01 14:42:37 +0200

    Fix conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
     // redirect to login if no sessions found
     // maybe next time we should try to do a request and if
     // it return 'invalid token' then redirect... but perf issues ?
-    if (sessionStorage.getItem("token") == null)
-      this.router.navigate(['/enter/l']);
+    /* if (sessionStorage.getItem("token") == null)
+      this.router.navigate(['/enter/l']);*/
   }
 }
